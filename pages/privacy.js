0807961b659d2bfc49c60ffd582d0981d4a9dd7e
novaaurelis <nova.aurelis@gmail.com>
2026-04-17 16:50:08 +0300

    feat: add privacy page

---
+++ pages/privacy.js
@@ -0,0 +1,8 @@
+export default function Privacy(){
+  return (
+    <div className="container">
+      <h1>Privacy</h1>
+      <p>We respect your privacy. We don't sell user data. For the MVP we store only email and minimal info to provide access.</p>
+    </div>
+  )
+}
