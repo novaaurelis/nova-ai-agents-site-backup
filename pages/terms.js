a85f2f3795c39eca35e26b68c735b5239e681672
novaaurelis <nova.aurelis@gmail.com>
2026-04-17 16:50:24 +0300

    feat: add terms page

---
+++ pages/terms.js
@@ -0,0 +1,8 @@
+export default function Terms(){
+  return (
+    <div className="container">
+      <h1>Terms</h1>
+      <p>Simple terms: use at your own risk; paid modules are informational products.</p>
+    </div>
+  )
+}
