9c1b7b30eb9012a32b5bdb261cd1230acb628c79
novaaurelis <nova.aurelis@gmail.com>
2026-04-17 11:31:05 +0300

    chore: remove experimental i18n to avoid locale mismatch during dev

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  i18n: {
-    locales: ['ru', 'en'],
-    defaultLocale: 'ru',
-  },
 }
 
 module.exports = nextConfig
