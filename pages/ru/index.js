269a4f02a8743975a0600378d93702d2e760d467
novaaurelis <nova.aurelis@gmail.com>
2026-04-23 02:10:37 +0300

    feat: replace popular models image with logos.png

--- pages/ru/index.js
+++ pages/ru/index.js
@@ -63,7 +63,7 @@ export default function RuHome(){
         <section className="models" aria-label="Модели">
           <h3 style={{textAlign: 'center'}}>Популярные модели</h3>
           <div style={{display:'flex',justifyContent:'center',marginTop:12}}>
-            <img src="/images/neurologos_collage.png" alt="neurologos collage" className="popular-illustration" style={{maxWidth:'100%',height:'auto',background:'transparent'}} />
+            <img src="/images/logos.png" alt="neurologos collage" className="popular-illustration" style={{maxWidth:'100%',height:'auto',background:'transparent'}} />
           </div>
         </section>
 
