f77da8c5bf93b5806bdf82f6e0923f2719bfa993
novaaurelis <nova.aurelis@gmail.com>
2026-04-23 12:51:07 +0300

    chore: remove legacy pages/modules.js to use modules/index.js dashboard

--- pages/modules.js
+++
@@ -1,9 +0,0 @@
-export default function Modules(){
-  return (
-    <div className="container">
-      <h1>Modules</h1>
-      <p>One-time modules will be available for purchase. Price: 299 ₽</p>
-      <p>For now you can register to get basic instructions for free.</p>
-    </div>
-  )
-}
